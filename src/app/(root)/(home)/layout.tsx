86ff65717f4bc96668a068f94df1448b66b0aa21
Beglerov Baurzhan <iowathe3rd@proton.me>
2024-04-11 02:51:43 +0500

    feat(layout, constants): added some const's and layout

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,6 +1,20 @@
+import Navbar from "@/components/shared/NavBar";
+import Sidebar from "@/components/shared/SideBar";
 import { PropsWithChildren } from "react";
 
 const Layout: React.FC<PropsWithChildren> = (props) => {
-  return <>{props.children}</>;
+  return (
+    <main className="relative">
+      <Navbar />
+
+      <div className="flex">
+        <Sidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{props.children}</div>
+        </section>
+      </div>
+    </main>
+  );
 };
 export default Layout;
